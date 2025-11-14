d6ef9085959e39296ba36d5a34a6fc3af46978b3
NathanKewley <nathan.kewley@gmail.com>
2025-11-14 22:43:00 +1100

    update to change deployment groups to deployment stacks for all deployments

--- src/azurite/lib/hooks/BashScript.py
+++ src/azurite/lib/hooks/BashScript.py
@@ -9,5 +9,5 @@ class Hook(HookBase):
 
     def execute_hook(self):
         self._logger.debug(f"Running Bash Hook: {self._arguments}")
-        if not self._subproc.run_command_exit_code(f"sh {self._arguments}") == 0:
+        if not self._subproc.run_command_exit_code(f"sh scripts/{self._arguments}") == 0:
             raise Exception(f"Bash Hook Returned non 0 result ({self._arguments})")
